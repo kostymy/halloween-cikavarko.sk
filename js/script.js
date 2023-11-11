46de8fbe898e1b4d0cf7e6feca49ca6b2b940dba
kostymy <97027776+kostymy@users.noreply.github.com>
2023-11-11 22:20:55 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    searchForm.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if(window.scrollY> 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+    document.querySelector('.header').classList.remove('active');
+
+}
+}
+
+var swiper = new Swiper(".products-slider"),{
+   spaceBetween: 20,
+   loop:true,
+   centeredSLides: true,
+   autoplay: {
+    display: 9500,
+    disableOnInteraction: false,
+},
+grabCursor: true,
+breakpoints: {
+    10: {
+        slidesPerView: 1,
+    },
+    768{
+        slidesPerView: 2,
+    },
+    991 {
+        slidesPerView: 3,
+    },
+},
+
+});
